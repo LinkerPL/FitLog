3b323b1ff5f84aaa77a4b30e06c2f65f5537b1b0
Paweł <pawel.lewandowski.kj@gmail.com>
2018-01-27 19:00:22 +0100

    first commit

---
+++ src/main/java/pl/fitlog/FitlogApplication.java
@@ -0,0 +1,12 @@
+package pl.fitlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlogApplication.class, args);
+	}
+}
