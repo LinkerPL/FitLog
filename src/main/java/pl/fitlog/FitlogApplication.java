d127e84fa443d09952237c77f6bf999391c77485
Paweł <pawel.lewandowski.kj@gmail.com>
2018-02-05 21:09:53 +0100

    model classes

--- src/main/java/pl/fitlog/FitlogApplication.java
+++ src/main/java/pl/fitlog/FitlogApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FitlogApplication.class, args);
+    }
 }
---
+++ src/main/java/pl/fitlog/model/ProductInMeal.java
@@ -0,0 +1,9 @@
+package pl.fitlog.model;
+
+/**
+ *
+ * @author linker
+ */
+public class ProductInMeal {
+    
+}
